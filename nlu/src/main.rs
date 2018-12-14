00720838e766a7e010991d1d2d52ceb46cbaa71a
William Myers <mwilliammyers@gmail.com>
2018-12-13 20:41:39 -0700

    Reconfigure nlu

--- nlu/src/main.rs
+++ nlu/src/main.rs
@@ -1,38 +1,17 @@
-#[macro_use]
-extern crate warp;
-
 use std::env;
 
-use env_logger as logger;
-use log::*;
-use serde_derive::*;
+use serde_json as json;
 use snips_nlu_lib::SnipsNluEngine;
-use warp::Filter;
-
-#[derive(Deserialize, Serialize)]
-struct ParseRequest {
-    text: String,
-}
 
 fn main() {
-    if env::var_os("RUST_LOG").is_none() {
-        env::set_var("RUST_LOG", "nlu=info,warp=debug");
+    if let Some(query) = env::args().nth(1) {
+       let engine =  match env::args().nth(2) {
+            Some(path) =>  SnipsNluEngine::from_path(path).unwrap(),
+            _ =>  SnipsNluEngine::from_path("model/engine/").unwrap(),
+        };
+
+        let result = engine.parse(query.trim(), None).unwrap();
+       
+        println!("{}", json::to_string(&result).unwrap());
     }
-    logger::init();
-
-    let parse = path!("parse")
-        .and(warp::body::content_length_limit(1024 * 1024 * 1024))
-        .and(warp::body::json())
-        .map(|req: ParseRequest| {
-            // TODO: figure out how to borrow engine so we don't have to load it every time
-            let engine = SnipsNluEngine::from_path("../models/nlu/engine/").unwrap();
-            debug!("Loaded the nlu engine...");
-
-            let result = engine.parse(req.text.trim(), None).unwrap();
-            info!("{:?}", &result);
-
-            warp::reply::json(&result)
-        });
-
-    warp::serve(parse).run(([0, 0, 0, 0], 9001));
 }
